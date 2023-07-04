7b4214e5b04007d4cba24c937d4de2ba64933725
Jakub Bigolas <jbigolas@gmail.com>
2023-07-04 12:31:57 +0200

    little fixes

--- config/config.sh
+++ config/config.sh
@@ -1,2 +1,8 @@
 # Add here any global configuration to keep it in one file
-DMVN_PROJECTS_DIR="C:\Users\jakub.bigolas\OneDrive\configs\dmvn"
+#DMVN_PROJECTS_DIR="C:\Users\jakub.bigolas\OneDrive\configs\dmvn"
+
+DOCKER_IMAGES_DIR="C:\Users\jakub.bigolas\OneDrive\configs\pdocker"
+export DOCKER_IMAGES_DIR
+
+CSOB_SRC_DIR="C:\workdir\csob\git"
+export CSOB_SRC_DIR
--- modules/dock_maven/domain/process/instance/process_instance.py
+++ modules/dock_maven/domain/process/instance/process_instance.py
@@ -32,16 +32,16 @@ class ProcessInstance:
         mvn_clean                        = self.__evaluate(self.__data.mvn_clean)
 
         print("Context params:")
-        print(" - package                          ={}  ({})".format(package, self.__data.package))
-        print(" - app_name                         ={}  ({})".format(app_name, self.__data.app_name))
-        print(" - src_path                         ={}  ({})".format(src_path, self.__data.src_path))
-        print(" - mvn_image_name                   ={}  ({})".format(mvn_image_name, self.__data.mvn_image_name))
-        print(" - target_path                      ={}  ({})".format(target_path, self.__data.target_path))
-        print(" - mvn_container_env_app_name       ={}  ({})".format(mvn_container_env_app_name, self.__data.mvn_container_env_app_name))
-        print(" - mvn_container_env_target_dir     ={}  ({})".format(mvn_container_env_target_dir, self.__data.mvn_container_env_target_dir))
-        print(" - mvn_container_env_build_cmd      ={}  ({})".format(mvn_container_env_build_cmd, self.__data.mvn_container_env_build_cmd))
-        print(" - mvn_container_env_build_profiles ={}  ({})".format(mvn_container_env_build_profiles, self.__data.mvn_container_env_build_profiles))
-        print(" - mvn_clean                        ={}".format(mvn_clean))
+        print(" - package                          ={}  ({})".format(package                          , self.__data.package))
+        print(" - app_name                         ={}  ({})".format(app_name                         , self.__data.app_name))
+        print(" - src_path                         ={}  ({})".format(src_path                         , self.__data.src_path))
+        print(" - mvn_image_name                   ={}  ({})".format(mvn_image_name                   , self.__data.mvn_image_name))
+        print(" - target_path                      ={}  ({})".format(target_path                      , self.__data.target_path))
+        print(" - mvn_container_env_app_name       ={}  ({})".format(mvn_container_env_app_name       , self.__data.mvn_container_env_app_name))
+        print(" - mvn_container_env_target_dir     ={}  ({})".format(mvn_container_env_target_dir     , self.__data.mvn_container_env_target_dir))
+        print(" - mvn_container_env_build_cmd      ={}  ({})".format(mvn_container_env_build_cmd      , self.__data.mvn_container_env_build_cmd))
+        print(" - mvn_container_env_build_profiles ={}  ({})".format(mvn_container_env_build_profiles , self.__data.mvn_container_env_build_profiles))
+        print(" - mvn_clean                        ={}"      .format(mvn_clean))
         print()
 
         target_dir = target_path + "/target"
@@ -49,29 +49,30 @@ class ProcessInstance:
         self.__app_context.recreate_dir(target_dir)
         print()
 
-        pom_path = f"{src_path}/pom.xml"
-        print(f"Clean source {src_path}")
-        self.__execute_cmd(f"mvn clean -f '{pom_path}'")
-        print()
+        if mvn_clean and mvn_clean.lower() == "true":
+            pom_path = f"{src_path}/pom.xml"
+            print(f"Clean source {src_path}")
+            self.__execute_cmd(f"mvn clean -f '{pom_path}'")
+            print()
 
 
-        mvn_container_name = f"mvn-builder-{app_name}"
         print("Starting builder container $mvn_container_name")
+        print()
+        mvn_container_name = f"mvn-builder-{app_name}"
         self.__execute_cmd(f"docker kill '{mvn_container_name}' || exit 0")
         self.__execute_cmd(f"docker rm   '{mvn_container_name}' || exit 0")
         command = ""
         command = self.__append_cmd(command, f"docker run  --rm"                                                                               )
-        command = self.__append_cmd(command, f"-e 'APP_NAME={mvn_container_env_app_name}'"       , mvn_container_env_app_name            )
-        command = self.__append_cmd(command, f"-e 'TARGET_DIR={mvn_container_env_target_dir}'"     , mvn_container_env_target_dir          )
-        command = self.__append_cmd(command, f"-e 'BUILD_CMD={mvn_container_env_build_cmd}'"      , mvn_container_env_build_cmd           )
+        command = self.__append_cmd(command, f"-e 'APP_NAME={mvn_container_env_app_name}'"             , mvn_container_env_app_name            )
+        command = self.__append_cmd(command, f"-e 'TARGET_DIR={mvn_container_env_target_dir}'"         , mvn_container_env_target_dir          )
+        command = self.__append_cmd(command, f"-e 'BUILD_CMD={mvn_container_env_build_cmd}'"           , mvn_container_env_build_cmd           )
         command = self.__append_cmd(command, f"-e 'BUILD_PROFILES={mvn_container_env_build_profiles}'" , mvn_container_env_build_profiles      )
         command = self.__append_cmd(command, f"-v '{src_path}:/usr/src/external/{app_name}'"           , src_path and app_name                 )
         command = self.__append_cmd(command, f"-v '{mvn_container_name}:/usr/src/mymaven/{app_name}'"  , mvn_container_name and app_name       )
         command = self.__append_cmd(command, f"-v '{target_path}:/build'"                              , target_path                           )
-        command = self.__append_cmd(command, f"-v m2cache:/root/.m2"                                                                            )
+        command = self.__append_cmd(command, f"-v m2cache:/root/.m2"                                                                           )
         command = self.__append_cmd(command, f"--name '{mvn_container_name}' '{mvn_image_name}'"       , mvn_container_name and mvn_image_name )
         self.__execute_cmd(command)
-        print()
 
         self.__print_end_process(start_at)
 
@@ -111,6 +112,7 @@ class ProcessInstance:
         end_at = time.time()
         delta = timedelta(milliseconds=(end_at - start_at))
         process_time = str(delta)
-        if len(process_time) == 7:
+        if len(process_time) > 2 and process_time[2] == ":":
             process_time = "0" + process_time
         print("End process in time {}".format(process_time))
+        print()
